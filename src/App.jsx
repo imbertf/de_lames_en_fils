c379548f2e9aa82b703dd97fac9f7af8d52c7738
flo <f.imbert4@gmail.com>
2023-03-20 11:12:40 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./pages/home";
 
 const App = () => {
-    return <div></div>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default App;
