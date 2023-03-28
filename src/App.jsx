9aeb4270ebe0cea79f91294ed8b24ef05338d4dd
flo <f.imbert4@gmail.com>
2023-03-28 16:57:53 +0200

    google analytics added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./pages/home";
 import Products from "./pages/products";
 import Pollux from "./pages/products/pollux";
 import Apero from "./pages/products/apero";
 import Stages from "./pages/stages";
-// import Interns from "./pages/interns";
 import Contact from "./pages/contact";
 import About from "./pages/about";
 import NotFound from "./pages/notFound";
+import ReactGA from "react-ga";
+
+const TRACKING_ID = "G-DXB8S7CFBS";
+ReactGA.initialize(TRACKING_ID);
 
 const App = () => {
+    useEffect(() => {
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    }, []);
     return (
         <BrowserRouter>
             <Routes>
@@ -19,7 +25,6 @@ const App = () => {
                 <Route path="/pollux" element={<Pollux />} />
                 <Route path="/apero" element={<Apero />} />
                 <Route path="/stages" element={<Stages />} />
-                {/* <Route path="/stagiaires" element={<Interns />} /> */}
                 <Route path="/contact" element={<Contact />} />
                 <Route path="/a-propos" element={<About />} />
                 <Route path="*" element={<NotFound />} />
